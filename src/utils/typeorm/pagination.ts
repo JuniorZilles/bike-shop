de75b43a11ac58e8d89951d69e3488d2d913e869
JuniorZilles <junior_zilles@hotmail.com>
2023-04-18 20:37:48 -0300

    fix: pagination from query params

--- src/utils/typeorm/pagination.ts
+++ src/utils/typeorm/pagination.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export default class Pagination {
   @ApiProperty()
   @IsOptional()
+  @Transform((params) => Number(params.value))
   @IsNumber()
   limit?: number;
 
   @ApiProperty()
   @IsOptional()
+  @Transform((params) => Number(params.value))
   @IsNumber()
   offset?: number;
 }
