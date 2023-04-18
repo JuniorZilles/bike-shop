4e85c86e1d1591dd3d5732ae87e19a1b2a6fc83a
JuniorZilles <junior_zilles@hotmail.com>
2023-04-18 20:38:27 -0300

    feat: adds findAllClient return model

--- src/client/dto/search.dto.ts
+++ src/client/dto/search.dto.ts
@@ -1,6 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import Pagination from '../../utils/typeorm/pagination';
+import Client from '../entities/client.entity';
 import IQueryDTO from './query.dto';
 
-export default interface ISearchClientDTO extends Pagination {
+export interface ISearchClientDTO extends Pagination {
   where?: IQueryDTO;
+}
+
+export class FindAllClient {
+  @ApiProperty()
+  totalResults: number;
+
+  @ApiProperty()
+  items: Client[];
+
+  @ApiProperty()
+  limit: number;
+
+  @ApiProperty()
+  offset: number;
 }
