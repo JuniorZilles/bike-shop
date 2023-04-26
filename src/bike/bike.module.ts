8ea2f579141edc86394477250a63b42910168839
JuniorZilles <junior_zilles@hotmail.com>
2023-04-26 20:26:21 -0300

    fix: import clientRepository

--- src/bike/bike.module.ts
+++ src/bike/bike.module.ts
@@ -5,10 +5,11 @@ import BikeController from './bike.controller';
 import Client from '../client/entities/client.entity';
 import Bike from './entities/bike.entity';
 import ClientRepository from '../client/repository/implementation/ClientRepository';
+import BikeRepository from './repository/implementation/BikeRepository';
 
 @Module({
   controllers: [BikeController],
-  providers: [BikeService],
-  imports: [TypeOrmModule.forFeature([Client, Bike]), ClientRepository]
+  providers: [ClientRepository, BikeRepository, BikeService],
+  imports: [TypeOrmModule.forFeature([Client, Bike])]
 })
 export default class BikeModule {}
