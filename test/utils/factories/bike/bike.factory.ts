e5a346c1f82ff7482ffad381173432be6f5078f4
JuniorZilles <junior_zilles@hotmail.com>
2023-04-27 20:01:20 -0300

    refactor: remove unused data

--- test/utils/factories/bike/bike.factory.ts
+++ test/utils/factories/bike/bike.factory.ts
@@ -16,20 +16,3 @@ export const invalidBike = {
   nr: '12456798',
   rimSize: '29'
 };
-
-export const updateValidBike = (clientId: string) => ({
-  displayName: faker.commerce.productName(),
-  brand: faker.commerce.product(),
-  clientId,
-  color: faker.color.human(),
-  nr: faker.random.numeric(10),
-  rimSize: faker.datatype.number({ max: 29, min: 24 })
-});
-
-export const updateInvalidBike = {
-  displayName: 'HDS 2021',
-  brand: 'OGGI',
-  color: 'Red',
-  nr: '12456798',
-  rimSize: '29'
-};
