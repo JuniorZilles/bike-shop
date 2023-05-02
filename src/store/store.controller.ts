9db66fb6510c2c5c1d87837e402507a4b0044193
JuniorZilles <junior_zilles@hotmail.com>
2023-05-02 20:16:06 -0300

    feat[store]: adds swagger details

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,33 +1,65 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  Query,
+  HttpCode,
+  UseInterceptors,
+  ClassSerializerInterceptor
+} from '@nestjs/common';
+import {
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse
+} from '@nestjs/swagger';
 import StoreService from './store.service';
 import CreateStoreDto from './dto/create-store.dto';
 import UpdateStoreDto from './dto/update-store.dto';
+import IQueryDTO from './dto/query.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('store')
 export default class StoreController {
   constructor(private readonly storeService: StoreService) {}
 
   @Post()
+  @ApiCreatedResponse()
+  @ApiConflictResponse()
   create(@Body() createStoreDto: CreateStoreDto) {
     return this.storeService.create(createStoreDto);
   }
 
   @Get()
-  findAll() {
-    return this.storeService.findAll();
+  @ApiOkResponse()
+  findAll(@Query() payload: IQueryDTO) {
+    return this.storeService.findAll(payload);
   }
 
   @Get(':id')
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
   findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.storeService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateStoreDto: UpdateStoreDto) {
     return this.storeService.update(id, updateStoreDto);
   }
 
   @Delete(':id')
+  @HttpCode(204)
+  @ApiNotFoundResponse()
+  @ApiNoContentResponse()
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.storeService.update(id, { isActive: false });
   }
