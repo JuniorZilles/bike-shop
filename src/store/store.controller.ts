c11bfe4c58d5e0def9aafbc35f70170b50baba2e
JuniorZilles <junior_zilles@hotmail.com>
2023-05-02 19:42:58 -0300

    feat[store]: improve controller param id

--- src/store/store.controller.ts
+++ src/store/store.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import StoreService from './store.service';
 import CreateStoreDto from './dto/create-store.dto';
 import UpdateStoreDto from './dto/update-store.dto';
@@ -18,17 +18,17 @@ export default class StoreController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.storeService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.storeService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStoreDto: UpdateStoreDto) {
-    return this.storeService.update(+id, updateStoreDto);
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updateStoreDto: UpdateStoreDto) {
+    return this.storeService.update(id, updateStoreDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.storeService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.storeService.remove(id);
   }
 }
