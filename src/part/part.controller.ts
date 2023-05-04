7bccd2a94dca27b9465a9eb59a7604152612933d
JuniorZilles <junior_zilles@hotmail.com>
2023-05-04 20:02:04 -0300

    feat: adds basic configs

--- src/part/part.controller.ts
+++ src/part/part.controller.ts
@@ -1,34 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  HttpCode,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+  Query
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse } from '@nestjs/swagger';
 import PartService from './part.service';
 import CreatePartDto from './dto/create-part.dto';
 import UpdatePartDto from './dto/update-part.dto';
+import IQueryDTO from './dto/query.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('part')
 export default class PartController {
   constructor(private readonly partService: PartService) {}
 
   @Post()
+  @ApiCreatedResponse()
+  @ApiNotFoundResponse()
   create(@Body() createPartDto: CreatePartDto) {
     return this.partService.create(createPartDto);
   }
 
   @Get()
-  findAll() {
-    return this.partService.findAll();
+  @ApiOkResponse()
+  findAll(@Query() payload: IQueryDTO) {
+    return this.partService.findAll(payload);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.partService.findOne(+id);
+  @ApiOkResponse()
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.partService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePartDto: UpdatePartDto) {
-    return this.partService.update(+id, updatePartDto);
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() updatePartDto: UpdatePartDto) {
+    return this.partService.update(id, updatePartDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.partService.remove(+id);
+  @HttpCode(204)
+  @ApiNotFoundResponse()
+  @ApiNoContentResponse()
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.partService.remove(id);
   }
 }
