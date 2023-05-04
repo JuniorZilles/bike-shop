4815df83cffaa6533555b7bcdd057f354bd0cb05
JuniorZilles <junior_zilles@hotmail.com>
2023-05-04 20:55:27 -0300

    style: typeorm cycle import issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'class-methods-use-this': 'off'
+    'class-methods-use-this': 'off',
+    'import/no-cycle': 'off'
   }
 }
