237295763efdc0b6fc2219db3c43a819cb2f77b2
JuniorZilles <junior_zilles@hotmail.com>
2023-05-08 21:51:19 -0300

    feat: adds service not found message

--- src/utils/constants/errorMessages.ts
+++ src/utils/constants/errorMessages.ts
@@ -3,5 +3,6 @@ export const userNotFound = 'User Not Found';
 export const bikeNotFound = 'Bike Not Found';
 export const partNotFound = 'Part Not Found';
 export const mechanicNotFound = 'Mechanic Not Found';
+export const serviceNotFound = 'Service Not Found';
 
 export const emailInUse = 'Email already in use';
