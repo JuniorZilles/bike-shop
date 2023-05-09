cd2b68c25b05da53a0134e06faee2870d3a93f6e
JuniorZilles <junior_zilles@hotmail.com>
2023-05-08 21:55:25 -0300

    fix: solves error on test

--- src/service/service.controller.spec.ts
+++ src/service/service.controller.spec.ts
@@ -1,7 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import ServiceController from './service.controller';
 import ServiceService from './service.service';
 import ServiceItensService from './service-itens.service';
+import ServiceRepository from './repository/implementation/ServiceRepository';
+import Service from './entities/service.entity';
+import MechanicRepository from '../mechanic/repository/implementation/MechanicRepository';
+import PartRepository from '../part/repository/implementation/PartRepository';
+import BikeRepository from '../bike/repository/implementation/BikeRepository';
+import ServiceItemRepository from './repository/implementation/ServiceItemRepository';
+import Bike from '../bike/entities/bike.entity';
+import Mechanic from '../mechanic/entities/mechanic.entity';
+import Part from '../part/entities/part.entity';
+import ServiceItem from './entities/serviceItem.entity';
 
 describe('ServiceController', () => {
   let controller: ServiceController;
@@ -9,7 +20,35 @@ describe('ServiceController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ServiceController],
-      providers: [ServiceService, ServiceItensService]
+      providers: [
+        {
+          provide: getRepositoryToken(Mechanic),
+          useFactory: jest.fn()
+        },
+        MechanicRepository,
+        {
+          provide: getRepositoryToken(Part),
+          useFactory: jest.fn()
+        },
+        PartRepository,
+        {
+          provide: getRepositoryToken(Bike),
+          useFactory: jest.fn()
+        },
+        BikeRepository,
+        {
+          provide: getRepositoryToken(ServiceItem),
+          useFactory: jest.fn()
+        },
+        ServiceItemRepository,
+        {
+          provide: getRepositoryToken(Service),
+          useFactory: jest.fn()
+        },
+        ServiceRepository,
+        ServiceService,
+        ServiceItensService
+      ]
     }).compile();
 
     controller = module.get<ServiceController>(ServiceController);
