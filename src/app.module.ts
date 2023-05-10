59844e4cfce954c10c7405707c2b7460a26d2c9d
JuniorZilles <junior_zilles@hotmail.com>
2023-05-10 20:04:22 -0300

    fix: module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 
 import StoreModule from './store/store.module';
 import ClientModule from './client/client.module';
-import { ServiceModule } from './service/service.module';
-import { FeedbackModule } from './feedback/feedback.module';
+import ServiceModule from './service/service.module';
+import FeedbackModule from './feedback/feedback.module';
 import DatabaseModule from './database/database.module';
 import BikeModule from './bike/bike.module';
 import PartModule from './part/part.module';
-import { MechanicModule } from './mechanic/mechanic.module';
+import MechanicModule from './mechanic/mechanic.module';
 
 @Module({
   imports: [
