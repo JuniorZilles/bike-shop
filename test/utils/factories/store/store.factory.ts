a314cb5bb28c5bac4afb7c9101884eb3a2f44187
JuniorZilles <junior_zilles@hotmail.com>
2023-05-11 20:19:09 -0300

    feat: adds factory store for testing

---
+++ test/utils/factories/store/store.factory.ts
@@ -0,0 +1,45 @@
+import { faker } from '@faker-js/faker';
+
+export const validStore = () => ({
+  city: faker.address.cityName(),
+  complement: faker.address.secondaryAddress(),
+  displayName: faker.company.name(),
+  neighborhood: faker.address.county(),
+  number: faker.address.buildingNumber(),
+  street: faker.address.street(),
+  phone: faker.phone.number('+55 ## #####-####'),
+  state: faker.address.stateAbbr(),
+  zipCode: faker.address.zipCode('#####-###'),
+  latitude: faker.address.latitude(),
+  longitude: faker.address.longitude(),
+  email: faker.internet.email(),
+  password: '^)3.Z<Hu6mRfUsXq'
+});
+
+export const invalidStore = {
+  city: 'Dois Irmãos',
+  complement: 'em frente ao posto shell',
+  displayName: 'Top Bike',
+  email: 'topbike',
+  neighborhood: 'centro',
+  number: '300',
+  street: 'Av. Irineu Becker',
+  phone: '+55 51 99173-',
+  state: 'TU',
+  zipCode: '93950-000',
+  password: 's',
+  latitude: -209.579,
+  longitude: -510.089
+};
+
+export const updateValidStore = () => ({
+  neighborhood: faker.address.county(),
+  number: faker.address.buildingNumber(),
+  street: faker.address.street()
+});
+
+export const updateInvalidStore = {
+  street: 'Av. Irineu Becker',
+  phone: '+55 51 99173-',
+  state: 'TU'
+};
