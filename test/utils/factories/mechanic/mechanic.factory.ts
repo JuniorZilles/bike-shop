139daa1637c9d8e343fbabb73b2dd513b0eea3d2
JuniorZilles <junior_zilles@hotmail.com>
2023-05-15 20:42:49 -0300

    feat: add missing attributes

--- test/utils/factories/mechanic/mechanic.factory.ts
+++ test/utils/factories/mechanic/mechanic.factory.ts
@@ -2,10 +2,10 @@ import { faker } from '@faker-js/faker';
 
 export const validMechanic = (storeId: string) => ({
   email: faker.internet.email(),
-  hiringDate: '2022-11-14',
-  name: 'John Doe',
-  password: 'aksjdkasjmd65asd654',
-  phone: '+55 12 94866-2978',
+  hiringDate: faker.date.recent(),
+  name: faker.name.fullName(),
+  password: '^)3.Z<Hu6mRfUsXq',
+  phone: faker.phone.number('+55 ## #####-####'),
   storeId
 });
 
