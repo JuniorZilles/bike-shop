2b48407444369b0655ec2db8a4f08d1e132b52ac
JuniorZilles <junior_zilles@hotmail.com>
2023-05-16 20:09:05 -0300

    fix: update dto

--- src/mechanic/dto/update-mechanic.dto.ts
+++ src/mechanic/dto/update-mechanic.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/mapped-types';
-import CreateMechanicDto from './create-mechanic.dto';
+import { IsDateString, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export default class UpdateMechanicDto extends PartialType(CreateMechanicDto) {}
+export default class UpdateMechanicDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  storeId?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  @IsPhoneNumber('BR')
+  phone?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsDateString()
+  hiringDate?: Date;
+
+  isActive?: boolean;
+}
--- src/store/dto/update-store.dto.ts
+++ src/store/dto/update-store.dto.ts
@@ -1,4 +1,66 @@
-import { PartialType } from '@nestjs/mapped-types';
-import CreateStoreDto from './create-store.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsLatitude, IsLongitude, IsNumber, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
+import countryStates from '../../utils/constants/countryStates';
 
-export default class UpdateStoreDto extends PartialType(CreateStoreDto) {}
+export default class UpdateStoreDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  displayName?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  street?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  number?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  complement?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  @IsLatitude()
+  latitude?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  @IsLongitude()
+  longitude?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  neighborhood?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  zipCode?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  @IsEnum(countryStates)
+  state?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  @IsPhoneNumber('BR')
+  phone?: string;
+
+  isActive?: boolean;
+}
