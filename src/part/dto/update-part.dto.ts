dfcb2134c974b662d09167a1d20bc802af1d05d4
JuniorZilles <junior_zilles@hotmail.com>
2023-05-17 20:20:45 -0300

    fix: adjust rule for update

--- src/part/dto/update-part.dto.ts
+++ src/part/dto/update-part.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/mapped-types';
-import CreatePartDto from './create-part.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 
-export default class UpdatePartDto extends PartialType(CreatePartDto) {}
+export default class UpdatePartDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  storeId: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  manufacturer?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  displayName?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
