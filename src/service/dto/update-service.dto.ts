6f665fbad9bd2ac7c08b4900ee1ee63d50e7980f
JuniorZilles <junior_zilles@hotmail.com>
2023-05-22 19:42:03 -0300

    fix: dto for service update

--- src/service/dto/update-service.dto.ts
+++ src/service/dto/update-service.dto.ts
@@ -1,4 +1,23 @@
-import { PartialType } from '@nestjs/mapped-types';
-import CreateServiceDto from './create-service.dto';
+import { ArrayMaxSize, ArrayMinSize, IsArray, IsOptional, IsString, IsUUID, ValidateNested } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import CreateServiceAdditionalItensDto from './create-service-additiona-itens.dto';
 
-export default class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+export default class UpdateServiceDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsUUID()
+  mechanicId: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  description: string;
+
+  @ArrayMaxSize(10)
+  @ArrayMinSize(1)
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateServiceAdditionalItensDto)
+  additionalItens: CreateServiceAdditionalItensDto[];
+}
