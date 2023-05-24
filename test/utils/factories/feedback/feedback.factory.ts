168c5cba7de54c83f48f7c0f10feb8996c36e794
JuniorZilles <junior_zilles@hotmail.com>
2023-05-23 21:09:09 -0300

    fix: number on rating

--- test/utils/factories/feedback/feedback.factory.ts
+++ test/utils/factories/feedback/feedback.factory.ts
@@ -2,21 +2,21 @@ import { faker } from '@faker-js/faker';
 
 export const validFeedback = (serviceId: string) => ({
   description: faker.lorem.paragraph(),
-  rating: faker.random.numeric(1),
+  rating: Number(faker.random.numeric(1, { bannedDigits: ['0', '6', '7', '8', '9'] })),
   serviceId
 });
 
 export const invalidFeedback = {
   description: 5463541,
-  rating: '1'
+  rating: '0'
 };
 
 export const updateValidFeedback = () => ({
   description: faker.lorem.paragraph(),
-  rating: faker.random.numeric(1)
+  rating: Number(faker.random.numeric(1, { bannedDigits: ['0', '6', '7', '8', '9'] }))
 });
 
 export const updateInvalidFeedback = {
   description: 5463541,
-  rating: '1'
+  rating: '0'
 };
